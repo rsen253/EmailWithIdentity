f8860373a9cb67de924d55c61348b9b9a4e567d9
rsen253 <rsen253@gmail.com>
2016-06-17 18:00:16 +0530

    Added gmail account to send confirmation link to the email

---
+++ IdentityEmailConformation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityEmailConformation.Startup))]
+namespace IdentityEmailConformation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
